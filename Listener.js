dcff76c7cac621c6483a815f96b6264c22e95e05
khjgames <khjgames@gmail.com>
2017-01-01 18:29:42 -0500

    Update Listener.js

--- Listener.js
+++ Listener.js
@@ -1,7 +1,9 @@
 var Communications = require("Communications");
-for (var i in Game.rooms){
-var rooom = Game.rooms[i];
-rooom.find(FIND_HOSTILE_CREEPS){
-Communications.
+for (var a in Game.rooms){
+var rooom = Game.rooms[a];
+var kreeps = rooom.find(FIND_HOSTILE_CREEPS);
+for (var b in kreeps){
+var creep = kreeps[b];
+Communications.CheckMessenger(creep);//Listening.
 }
 }
